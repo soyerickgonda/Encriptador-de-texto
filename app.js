63290fd2f3fbea3e65f214c6446cf55074a6cbb6
soyerickgonda <soyerickgonda@users.noreply.github.com>
2024-02-26 21:03:28 -0500

    Se crea un encriptador/desencriptador de texto. El proyecto funcionara siempre que el texto ingresado cumpla lo sgte: -contenga alguna vocal -no este en mayuscula -no debe añadirse tildes

---
+++ app.js
@@ -0,0 +1,62 @@
+//FUNCION PARA ENCRIPTAR
+
+// let texto = "fenterlimescimesdaidenters poberr enternfrenterntair enterstenter dentersaifimesober y haibenterrlober cobernclufatimesdober cobern enterximestober!";
+//usamos la expresion regular de js /a/g --> la 'g' hace referencia a 'global'
+// function encriptar(texto) {
+//  if(texto !== null || texto !== undefined){
+//    // let texto = prompt('Ingrese un texto');
+
+//   //si el texto original contiene la vocal 'e' convertirlo a 'enter'
+//   if (texto.includes("e")) {
+//     texto = texto.replace(/e/g, "enter");
+//   }
+
+//   //si el texto original contiene la vocal 'i' convertirlo a 'imes'
+//   if (texto.includes("i")) {
+//     texto = texto.replace(/i/g, "imes");
+//   }
+
+//   //si el texto original contiene la vocal 'a' convertirlo a 'ai'
+//   if (texto.includes("a")) {
+//     texto = texto.replace(/a/g, "ai");
+//   }
+
+//   //si el texto original contiene la vocal 'o' convertirlo a 'ober'
+//   if (texto.includes("o")) {
+//     texto = texto.replace(/o/g, "ober");
+//   }
+
+//   //si el texto original contiene la vocal 'u' convertirlo a 'ufat'
+//   if (texto.includes("u")) {
+//     texto = texto.replace(/u/g, "ufat");
+//   }
+
+//   console.log(texto);
+//  }else {
+
+//  }
+// }
+// encriptar(texto);
+
+//FUNCION PARA DESENCRIPTAR
+// function desencriptar(texto) {
+//   //e
+//   if (texto.includes("enter")) {
+//     texto = texto.replace(/enter/g, "e");
+//   }
+//   if(texto.includes('imes')){
+//     texto = texto.replace(/imes/g, 'i')
+//   }
+//   if(texto.includes('ai')){
+//     texto = texto.replace(/ai/g, 'a')
+//   }
+//   if(texto.includes('ober')){
+//     texto = texto.replace(/ober/g, 'o')
+//   }
+//   if(texto.includes('ufat')){
+//     texto = texto.replace(/ufat/g, 'u')
+//   }
+//   console.log(texto);
+// }
+// desencriptar(texto);
+
